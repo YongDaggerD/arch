a2dd625dc0e1d613e76fa66253806d7571254d8c
Игорь <igor2001601@gmail.com>
2020-03-29 22:05:50 +0300

    training commit

--- src/components/Training/Training.jsx
+++ src/components/Training/Training.jsx
@@ -10,7 +10,7 @@ const Training = () => {
             <div className={cs.lower}>
                 <div className={cs.item}>
                     <div className={cs.picture}></div>
-                    <div className={cs.item-lower}></div>
+                    <div className={cs.lower_box}></div>
                 </div>
                 <div className={cs.item}>picrure</div>
                 <div className={cs.item}>picture</div>
